23c7830645183d342c460f44c121f7c1ad2fd508
PhamDuy01 <duyb1909891@student.ctu.edu.vn>
2023-02-24 02:04:48 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-
+const contactsRouter = require("./app/routes/contact.route");
 const app = express();
-
+app.use("/api/contacts", contactsRouter);
 app.use(cost());
 app.use(express.json());
 
