547326631b81176db5529430faa8ff3955a9ca25
PhamDuy01 <duyb1909891@student.ctu.edu.vn>
2023-02-24 02:15:54 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
-const express = require("express");
-const cors = require("cors");
-const contactsRouter = require("./app/routes/contact.route");
-const app = express();
-app.use("/api/contacts", contactsRouter);
-app.use(cost());
-app.use(express.json());
 
-app.get("/",(req,res) => {
-    res.json({ message: "Wellcome to contact book application."});
-});
+const contactsRouter = require("./app/routes/contact.route");
+
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
