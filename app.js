b0d9669b369f0c7b6724920b99423d57be80e8df
PhamDuy01 <duyb1909891@student.ctu.edu.vn>
2023-02-24 02:22:35 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 
 const contactsRouter = require("./app/routes/contact.route");
-
+const ApiError= require("./app/api-error");
 app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts", contactsRouter);
+app.use((req,res,next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+app.use((err,req,res,next) =>{
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;
